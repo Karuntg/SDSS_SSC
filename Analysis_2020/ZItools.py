dc2000d8031e4f4b085b57cd130ed910c64911e2
Zeljko Ivezic <ivezic@astro.washington.edu>
2020-06-11 17:12:58 -0700

    plotting tools

---
+++ Analysis_2020/ZItools.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+from astropy.table import Table
+
+# robust standard deviation
+def sigG(arr):
+    return 0.741*(np.quantile(arr, 0.75)-np.quantile(arr, 0.25))
+
+
+### PLOTS  
+
+# wrapper around plotdelMag below to use only two vectors instead of astropy Table 
+def plotdelMagArr(xVec, yVec, kw):
+    t = Table()
+    t[kw['Xstr']] = xVec
+    t[kw['Ystr']] = yVec
+    plotdelMag(t, kw)
+    return 
+
+def plotdelMag(d, kw):
+ 
+    print('medianAll:', np.median(d[kw['Ystr']]), 'std.dev.All:', sigG(d[kw['Ystr']]))
+    print('N=', np.size(d[kw['Ystr']]), 'min=', np.min(d[kw['Ystr']]), 'max=', np.max(d[kw['Ystr']]))
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.scatter(d[kw['Xstr']], d[kw['Ystr']], s=0.003, c='black') 
+    # binning
+    xBinM, nPtsM, medianBinM, sigGbinM = fitMedians(d[kw['Xstr']], \
+                                         d[kw['Ystr']], kw['XminBin'], kw['XmaxBin'], kw['nBin'], 1)
+    # plotting
+    if (kw['offset'] >= 0):
+        xL = np.linspace(kw['XminBin'], kw['XmaxBin'])
+        ax.plot(xL, 0*xL, '-', c='red', linewidth=3)
+        ax.plot(xL, 0*xL+kw['offset'], '--', c='red', linewidth=3)
+        ax.plot(xL, 0*xL-kw['offset'], '--', c='red', linewidth=3)
+    if (0):
+        ax.scatter(xBinM, medianBinM, s=30.0, c='black', alpha=0.8)
+        ax.scatter(xBinM, medianBinM, s=15.0, c='yellow', alpha=0.3)
+    #
+    TwoSigP = medianBinM + kw['Nsigma']*sigGbinM
+    TwoSigM = medianBinM - kw['Nsigma']*sigGbinM 
+    ax.plot(xBinM, TwoSigP, c='yellow', linewidth=3)
+    ax.plot(xBinM, TwoSigM, c='yellow', linewidth=3)
+    #
+    rmsBin = np.sqrt(nPtsM) / np.sqrt(np.pi/2) * sigGbinM
+    rmsP = medianBinM + rmsBin
+    rmsM = medianBinM - rmsBin
+    ax.plot(xBinM, rmsP, c='cyan', linewidth=3)
+    ax.plot(xBinM, rmsM, c='cyan', linewidth=3)
+    # 
+    ax.set_xlabel(kw['Xlabel'], fontsize=22)
+    ax.set_ylabel(kw['Ylabel'], fontsize=22)
+    ax.set_xlim(kw['Xmin'], kw['Xmax'])
+    ax.set_ylim(kw['Ymin'], kw['Ymax'])
+    plt.xticks(fontsize=22)
+    plt.yticks(fontsize=22)
+    plt.savefig(kw['plotName'], dpi=600)
+    print('saved plot as:', kw['plotName']) 
+    plt.show()
+    return
+ 
+
+# given vectors x and y, fit medians in bins from xMin to xMax, with Nbin steps,
+# and return xBin, medianBin, medianErrBin 
+def fitMedians(x, y, xMin, xMax, Nbin, verbose=1): 
+
+    # first generate bins
+    xEdge = np.linspace(xMin, xMax, (Nbin+1)) 
+    xBin = np.linspace(0, 1, Nbin)
+    nPts = 0*np.linspace(0, 1, Nbin)
+    medianBin = 0*np.linspace(0, 1, Nbin)
+    sigGbin = -1+0*np.linspace(0, 1, Nbin) 
+    for i in range(0, Nbin): 
+        xBin[i] = 0.5*(xEdge[i]+xEdge[i+1]) 
+        yAux = y[(x>xEdge[i])&(x<=xEdge[i+1])]
+        if (yAux.size > 0):
+            nPts[i] = yAux.size
+            medianBin[i] = np.median(yAux)
+            # robust estimate of standard deviation: 0.741*(q75-q25)
+            sigmaG = 0.741*(np.percentile(yAux,75)-np.percentile(yAux,25))
+            # uncertainty of the median: sqrt(pi/2)*st.dev/sqrt(N)
+            sigGbin[i] = np.sqrt(np.pi/2)*sigmaG/np.sqrt(nPts[i])
+        else:
+            nPts[i] = 0
+            medianBin[i] = 0
+            sigGbin[i] = 0
+        
+    if (verbose):
+        print('median:', np.median(medianBin[nPts>0]), 'std.dev:', np.std(medianBin[nPts>0]))
+
+    return xBin, nPts, medianBin, sigGbin
+
+
+
+
